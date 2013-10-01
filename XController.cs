3d5d41e5abf2416ddbb33306c89fe25a955c4dac
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2013-10-01 17:35:36 +0300

    Project files added

---
+++ XController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace Campus.Core
+{
+    public abstract class XController : Controller
+    {
+        /// <summary>
+        /// Method return information about another method supported by controller
+        /// </summary>
+        /// <returns></returns>
+        public virtual JsonResult Introspect()
+        {
+            //Default Introspect implementation
+
+            var type = this.GetType();
+
+            var methods = (from method in type.GetMethods()
+                           where
+                               method.ReturnType.BaseType == typeof(ActionResult) ||
+                               method.ReturnType == typeof(ActionResult)
+                           select IntrospectMethod(method)).ToList();
+
+            return Json(methods, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual ActionResult Index()
+        {
+            return Introspect();
+        }
+
+        private static dynamic IntrospectMethod(MethodInfo method)
+        {
+            return new
+                {
+                    method.Name,
+                    Parameters = method.GetParameters().Select(o => new
+                        {
+                            o.Name,
+                            Type = o.ParameterType.ToString()
+                        }).ToList()
+                };
+        }
+
+    }
+}
