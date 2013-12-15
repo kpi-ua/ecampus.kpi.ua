30b7b603710a888de687d1a68e35737624ba8fc7
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2013-12-15 16:17:19 +0200

    Vesrion changed

--- Campus.SDK/Client.cs
+++ Campus.SDK/Client.cs
@@ -1,22 +1,63 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Campus.SDK
 {
     /// <summary>
-    /// 
+    /// ecampus client
     /// </summary>
-    public class Client : AsyncClient
+    public class Client
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static IWebProxy Proxy { get; set; }
+
+        public const string ApiEndpoint = "http://api.ecampus.kpi.ua/";
+
+        private DateTime _authenticatedTime;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string SessionId { get; protected set; }
+
+        /// <summary>
+        /// by default session has expire time. This property retrn that your session is valid.
+        /// </summary>
+        public bool SessionIsActual
+        {
+            get { return _authenticatedTime.AddMinutes(18) > DateTime.Now; }
+        }
+
+        public Client()
+        {
+            SessionId = String.Empty;
+        }
+
         /// <summary>
         /// Authenticate user in ecampus
         /// </summary>
         /// <param name="login">User login</param>
         /// <param name="password">User password</param>
         /// <returns>Session id</returns>
-        public new string Authenticate(string login, string password)
+        public String Authenticate(string login, string password)
         {
-            var asyncResult = base.Authenticate(login, password);
-            return asyncResult.Result;
+            var url = String.Format("{0}?login={1}&password={2}", BuildUrl("User", "Auth"), login, password);
+
+            var result = Get(url);
+            SessionId = result.Data;
+
+            if (String.IsNullOrEmpty(SessionId))
+            {
+                throw new Exception("Access denied");
+            }
+
+            _authenticatedTime = DateTime.Now;
+
+            return SessionId;
         }
 
         /// <summary>
@@ -24,10 +65,9 @@ namespace Campus.SDK
         /// </summary>
         /// <param name="url"></param>
         /// <returns></returns>
-        public new Result Get(string url)
+        public Result Get(string url)
         {
-            var asyncResult = base.Request(url, HttpMethod.Get, null);
-            return asyncResult.Result;
+            return Request(url, HttpMethod.Get, null);
         }
 
         /// <summary>
@@ -37,10 +77,162 @@ namespace Campus.SDK
         /// <param name="method"></param>
         /// <param name="bytes"></param>
         /// <returns></returns>
-        public new Result Request(string url, HttpMethod method, byte[] bytes)
+        public Result Request(string url, HttpMethod method, byte[] bytes)
         {
-            var asyncResult = base.Request(url, method, bytes);
-            return asyncResult.Result;
+            var handler = new HttpClientHandler();
+
+            if (Proxy != null)
+            {
+                //CookieContainer = cookies,
+                //UseCookies = true,
+                handler.UseDefaultCredentials = false;
+                handler.Proxy = Proxy;
+                handler.UseProxy = true;
+            }
+
+            var client = new HttpClient(handler);
+
+            Task<String> result = null;
+
+            if (method == HttpMethod.Get)
+            {
+                result = client.GetStringAsync(url);
+            }
+            else if (method == HttpMethod.Head)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Options)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Post)
+            {
+                var task = client.PostAsync(url, new ByteArrayContent(bytes));
+                var responseMessage = task.Result;
+                result = responseMessage.Content.ReadAsStringAsync();
+            }
+            else if (method == HttpMethod.Put)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Trace)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Delete)
+            {
+                throw new NotImplementedException();
+            }
+
+            var json = result.Result;
+
+            return Result.Parse(json);
+        }
+        
+        #region Async
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public async Task<String> AuthenticateAsync(string login, string password)
+        {
+            var url = String.Format("{0}?login={1}&password={2}", BuildUrl("User", "Auth"), login, password);
+
+            var task = GetAsync(url);
+            SessionId = task.Result.Data;
+
+            if (String.IsNullOrEmpty(SessionId))
+            {
+                throw new Exception("Access denied");
+            }
+
+            _authenticatedTime = DateTime.Now;
+
+            return SessionId;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<Result> GetAsync(string url)
+        {
+            return await RequestAsync(url, HttpMethod.Get, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="method"></param>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public async Task<Result> RequestAsync(string url, HttpMethod method, byte[] bytes)
+        {
+            var handler = new HttpClientHandler();
+
+            if (Proxy != null)
+            {
+                //CookieContainer = cookies,
+                //UseCookies = true,
+                handler.UseDefaultCredentials = false;
+                handler.Proxy = Proxy;
+                handler.UseProxy = true;
+            }
+
+            var client = new HttpClient(handler);
+
+            Task<String> result = null;
+
+            if (method == HttpMethod.Get)
+            {
+                result = client.GetStringAsync(url);
+            }
+            else if (method == HttpMethod.Head)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Options)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Post)
+            {
+                var responseMessage = await client.PostAsync(url, new ByteArrayContent(bytes));
+                result = responseMessage.Content.ReadAsStringAsync();
+            }
+            else if (method == HttpMethod.Put)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Trace)
+            {
+                throw new NotImplementedException();
+            }
+            else if (method == HttpMethod.Delete)
+            {
+                throw new NotImplementedException();
+            }
+            return Result.Parse(await result);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Build URL for API method
+        /// </summary>
+        /// <param name="controller">Contorller name</param>
+        /// <param name="method">Method name</param>
+        /// <param name="arguments">Line of arguments must be start with ? symbol</param>
+        /// <returns>Url for method call</returns>
+        public static string BuildUrl(string controller, string method, string arguments = "")
+        {
+            return String.Format("{0}{1}/{2}{3}", ApiEndpoint, controller, method, arguments);
         }
     }
 }
