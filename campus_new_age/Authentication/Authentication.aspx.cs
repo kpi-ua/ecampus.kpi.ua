262fdd5bf3f01f434b4da95450869cb2b6c6e1f8
ernado-x <andrew.gubskiy@gmail.com>
2013-12-15 12:02:47 +0200

    Initial commit

---
+++ campus_new_age/Authentication/Authentication.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.IO;
+
+
+namespace campus_new_age.Authentication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private const string domain = "localhost:55897";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+                if ((Request.Cookies["Session"] != null) && (Request.Cookies["Session"].Value != "") && (Session["SaveIn"] != null) && (Convert.ToBoolean(Session["SaveIn"]) == true))
+                {
+                    Response.Redirect("Profile.aspx");
+                } else {
+                    User.Text = "Логін";
+                    Pass.Attributes["type"] = "password";
+                    Pass.Text = "Пароль";
+                    SaveIn.Checked = false;
+                }
+                
+            }
+
+
+        }
+
+        protected void Enter_Click(object sender, EventArgs e)
+        {
+            String req = String.Format("http://api.ecampus.kpi.ua/User/Auth?login={0}&password={1}", User.Text, Pass.Text);
+            Dictionary<string, string> respDictionary = GetJson(req);
+
+            if (respDictionary.ContainsKey("Data"))
+            {
+                if (SaveIn.Checked)
+                {
+                    HttpCookie myCookie = new HttpCookie("Session");
+                    myCookie.Value = Session.SessionID;
+                    myCookie.Expires = DateTime.Now.AddDays(1d);
+                    Response.Cookies.Add(myCookie);
+                    Session["SaveIn"] = true;
+                } else {
+                    Session["SaveIn"] = false;
+                }
+
+                Session["UserData"] = respDictionary["Data"].ToString();
+                
+                Response.Redirect("Profile.aspx");
+            }
+            else
+            {
+                Response.Redirect("http://" + domain + "/RNP/RNP_View.aspx");
+            }
+        }
+
+        protected void Cancel_Click(object sender, EventArgs e)
+        {
+            User.Text = "Логін";
+            Pass.Attributes["type"] = "password";
+            Pass.Text = "Пароль";
+            SaveIn.Checked = false;
+        }
+
+        public Dictionary<string, string> GetJson(String req)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+
+                WebProxy p = new WebProxy("10.13.100.13:3128", true);
+                p.Credentials = new NetworkCredential("kbis_user", "kbis13");
+                WebRequest.DefaultWebProxy = p;
+                client.Proxy = p;
+
+                var json = client.DownloadString(req);
+
+                var serializer = new JavaScriptSerializer();
+
+                return serializer.Deserialize<Dictionary<string, string>>(json);
+
+            }
+            catch (Exception ex)
+            {
+                return new Dictionary<string, string>();
+            }
+
+        }
+
+
+    }
+
+}
---
+++ campus_new_age/Authentication/Profile.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/SitePlusNav.Master" AutoEventWireup="true" CodeBehind="Profile.aspx.cs" Inherits="campus_new_age.Authentication.Success" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>Профіль</title>
+
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <div style="max-width: 80%; /*background-color: cadetblue; */ float: left;">
+        <h3 class="text-primary">Мій профіль</h3>
+        <div style="margin-left: 15px;">
+            <h4 class="text-muted">Персональні дані</h4>
+            <asp:Literal ID="PersData" runat="server"></asp:Literal>
+            <h4 class="text-muted">Дані за місцем навчання (роботи)</h4>
+            <asp:Literal ID="WorkData" runat="server"></asp:Literal>
+            <h4 class="text-muted">Додаткові функції</h4>
+            <asp:Literal ID="SpecFunc" runat="server"></asp:Literal>
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="right_column" runat="server">
+    <div id="right-sidebar">
+        <asp:Image ID="Photo" runat="server" />
+        <%--<img src="/Images/user_pic.png" />--%>
+        <ul>
+            <li><a href="#">
+                <img src="/Images/foto.png" />завантажити фото</a></li>
+            <li><a href="#">
+                <img src="/Images/parol.png" />змінити пароль</a></li>
+        </ul>
+    </div>
+    <!-- .right-sidebar -->
+</asp:Content>
+
