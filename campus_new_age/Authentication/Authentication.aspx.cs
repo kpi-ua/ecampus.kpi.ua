ac4e14017b23d30e4d3dd48a3ec67f3bc8e5a280
nikolay-m <nikolay.malenko@gmail.com>
2013-12-20 14:50:56 +0200

    Messages

--- campus_new_age/Authentication/Authentication.aspx.cs
+++ campus_new_age/Authentication/Authentication.aspx.cs
@@ -80,10 +80,10 @@ namespace campus_new_age.Authentication
             {
                 WebClient client = new WebClient();
 
-                //WebProxy p = new WebProxy("10.13.100.13:3128", true);
-                //p.Credentials = new NetworkCredential("kbis_user", "kbis13");
-                //WebRequest.DefaultWebProxy = p;
-                //client.Proxy = p;
+                WebProxy p = new WebProxy("10.13.100.13:3128", true);
+                p.Credentials = new NetworkCredential("kbis_user", "kbis13");
+                WebRequest.DefaultWebProxy = p;
+                client.Proxy = p;
 
                 var json = client.DownloadString(req);
 
