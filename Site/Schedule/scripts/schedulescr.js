151cd5f25bd8a4394ec5212f4eb098230169d87d
Alex Frost <alex.frost.ua@gmail.com>
2014-02-02 18:03:20 +0200

    Updated TimeTable

--- Site/Schedule/scripts/schedulescr.js
+++
@@ -1,8 +0,0 @@
-try{
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET","http://api.ecampus.kpi.ua", false);
-	xhr.send();
-}
-catch(e){
-	console.log(e);
-}
--- Site/TimeTable/TimeTableMain.aspx.cs
+++ Site/TimeTable/TimeTableMain.aspx.cs
@@ -10,48 +10,13 @@ namespace Site.TimeTable
 {
     public partial class TimeTableMain : System.Web.UI.Page
     {
-        private WebClient client;
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Page.IsPostBack)
-            {
-                string sessionId = Session["UserData"].ToString();
-                if (Session["UserData"] == null)
-                {
-                    throw new Exception("Bad Authorisation!");
-                }
 
-                client = new WebClient();
-                client.Encoding = System.Text.Encoding.UTF8;
-                var json = client.DownloadString("http://api.ecampus.kpi.ua/User/GetCurrentUser?sessionId=" + sessionId);
-                var serializer = new JavaScriptSerializer();
-                var objects = serializer.Deserialize<object>(json);
-            }
-            
-            
+
         }
 
-
-        protected void GetSubjects(int specialityId, int studyFormId, int course)
-        {
-            var json =
-                client.DownloadString("http://api.ecampus.kpi.ua/TimeTable/GetSubjects?sessionId=" +
-                                      Session["UserData"].ToString() + "&course=" + course.ToString(CultureInfo.InvariantCulture) + "&specialityId=" +
-                                      specialityId + "&dcStudyFormId=" + studyFormId);
-        }
-
-        protected void TimeTableFiller(Dictionary<string,List<string>> timeTableDictionary)
-        {
-            foreach (string key in timeTableDictionary.Keys)
-            {
-                for (int j = 0; j < timeTableDictionary[key].Count; j++)
-                {
-                    Control cont = FindControl(key + j.ToString());
-                    Label lab = (Label) cont;
-                    lab.Text = timeTableDictionary[key][j];
-                }
-            }
-        }
     }
 }
