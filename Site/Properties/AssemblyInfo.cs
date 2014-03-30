2a52d63787599f99d5d0fb57ee766a0a98a44981
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2014-03-30 09:59:24 +0300

    Packages updated. Added Campus.Common package

--- Site/Properties/AssemblyInfo.cs
+++ Site/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ecampus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DOIS")]
+[assembly: AssemblyCompany("NTUU KPI")]
 [assembly: AssemblyProduct("ecampus")]
-[assembly: AssemblyCopyright("Copyright © DOIS 2014")]
+[assembly: AssemblyCopyright("Copyright NTUU KPI © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.155")]
