60c9c3b91e253c61902390bdfac39e311e41d8f4
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2014-05-29 19:42:26 +0300

    Form fix

--- Site/Site.Master.designer.cs
+++ Site/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Site {
         protected global::System.Web.UI.WebControls.Literal UserName;
         
         /// <summary>
+        /// form control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form;
+        
+        /// <summary>
         /// script_manager control.
         /// </summary>
         /// <remarks>
