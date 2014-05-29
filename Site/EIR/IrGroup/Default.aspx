6f5dd70daeba5d20473b02547919f69cca4c8dc2
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2014-05-29 12:14:40 +0300

    Created page ir group view

---
+++ Site/EIR/IrGroup/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Site.EIR.IrGroup.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="body" runat="server">
+    <h1>Hello world!</h1>
+    <div id="out_json" runat="server"></div>
+</asp:Content>
---
+++ Site/EIR/IrGroup/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Site.EIR.IrGroup
+{
+    public partial class Default : SitePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var client = new Campus.SDK.Client();
+            var url = Campus.SDK.Client.BuildUrl("IrGroup", "GetAllPrivateIrGroups", new { SessionId });
+            var result = client.Get(url);
+            out_json.InnerText = result.Data.ToString();
+        }
+    }
+}
