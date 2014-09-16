744dfe5a8d3a3dc4488cfc90f9e4269dc480e813
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2014-09-16 17:47:41 +0300

    Added TestContainer project

---
+++ TestContainer/ExampleUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestContainer
+{
+    [TestClass]
+    public class ExampleUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1 == 1, true);
+        }
+    }
+}
