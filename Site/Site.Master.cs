3a1b4409c9eb7bd4a53f2ca73b6faf2781786dad
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2014-10-07 23:19:10 +0300

    Package update. Fix problem when user hove no employee-record (when he is a student).

--- Site/Site.Master.cs
+++ Site/Site.Master.cs
@@ -1,9 +1,6 @@
-﻿using System.IO;
-using System.Linq;
-using System.Text;
-using System.Web.UI.WebControls;
-using Core;
+﻿using Core;
 using System;
+using System.Linq;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 
@@ -11,12 +8,31 @@ namespace Site
 {
     public partial class SiteMaster : MasterPage
     {
-        public int A;
+        public int A;  //what is is?
+
         protected SitePage SitePage
         {
             get { return this.Page as SitePage; }
         }
-        
+
+        public string Position
+        {
+            get
+            {
+                var e = this.SitePage.CurrentUser.Employees.FirstOrDefault();
+                return e == null ? String.Empty : e.Position;
+            }
+        }
+
+        public string AcademicDegree
+        {
+            get
+            {
+                var e = this.SitePage.CurrentUser.Employees.FirstOrDefault();
+                return e == null ? String.Empty : e.AcademicDegree;
+            }
+        }
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -38,14 +54,14 @@ namespace Site
 
                 form.Controls.Add(hiddenField1);
 
-               // individual_plan.Visible = SitePage.CurrentUser.Employees.Any(o => o.Position.Contains("Викладач"));
+                // individual_plan.Visible = SitePage.CurrentUser.Employees.Any(o => o.Position.Contains("Викладач"));
             }
             catch
             {
                 UserName.Text = "Ошибка при загрузке страницы!!!";
             }
         }
-        
+
         public void carousel_profile_button_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/Default.aspx");
@@ -95,6 +111,5 @@ namespace Site
         {
             Response.Redirect("~/Default.aspx");
         }
-
     }
 }
