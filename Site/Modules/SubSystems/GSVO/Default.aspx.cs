1eab1b8c2ab013bac380508c30f1296973c7e998
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2014-11-26 20:59:08 +0200

    Fixed page parent class

--- Site/Modules/SubSystems/GSVO/Default.aspx.cs
+++ Site/Modules/SubSystems/GSVO/Default.aspx.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using Core;
+using System;
 
 namespace Site.Modules.SubSystems.GSVO
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class Default : SitePage
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected override void OnLoad(EventArgs e)
         {
-
+            base.OnLoad(e);
         }
     }
 }
