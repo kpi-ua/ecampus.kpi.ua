ece2cdc93ebf9dc6329a1fc6fe605d9f3bc59ff4
DarthPanda <a89523698>
2014-12-01 11:19:48 +0200

    adding script

---
+++ Site/Modules/SubSystems/GSVO/Table.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.UI;
+using AjaxControlToolkit.HTMLEditor.ToolbarButton;
+using Core;
+using Campus.SDK;
+using Campus.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Site.Modules.SubSystems.GSVO
+{
+    public partial class Table : Core.SitePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
