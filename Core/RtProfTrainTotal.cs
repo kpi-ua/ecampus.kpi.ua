2533497338088b490854d37578419c1f3dbffc10
Oleg <oushko@yandex.ua>
2014-12-11 13:03:04 +0200

    fix Arsen bug

---
+++ Core/RtProfTrainTotal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Campus.Common
+{
+    public class RtProfTrainTotal
+    {
+        public int RtProfTrainTotalId { get; set; }
+        public string TotalShifr { get; set; }
+        public string Name { get; set; }
+    }
+}
