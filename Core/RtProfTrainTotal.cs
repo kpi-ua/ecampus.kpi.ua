357067e009410fa801e43ccf33924e5f81f91496
Alex Frost <alex.frost.ua@gmail.com>
2014-12-11 13:03:22 +0200

    Кто-то забыл закоммитить RtProfTrainTotal.cs, починил. Нужно исправить типы полей.

---
+++ Core/RtProfTrainTotal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Campus.Common
+{
+    public class RtProfTrainTotal
+    {
+        //no data
+        public object RtProfTrainTotalId { get; set; }
+        public object Name { get; set; }
+        public object TotalShifr { get; set; }
+    }
+}
