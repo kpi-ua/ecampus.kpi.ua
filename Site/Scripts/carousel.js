786f63fbc370297638514a492118e90d3dc5d24d
Dmitriy <dmitriy.kozyatinskiy@gmail.com>
2015-01-26 23:52:19 +0200

    Update for the carousel

---
+++ Site/Scripts/carousel.js
@@ -0,0 +1,36 @@
+﻿$('.carousel').slick({
+    arrows: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    variableWidth: true,
+    draggable: false,
+    infinite: false,
+    nextArrow: $(".glyphicon-chevron-left"),
+    prevArrow: $(".glyphicon-chevron-right"),
+    responsive: [
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+    ]
+});
