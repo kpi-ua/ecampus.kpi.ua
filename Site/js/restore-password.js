f5141448035f794831e512e9ff69ca993fa96409
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-06-12 00:13:58 +0300

    Cleanup styles. Implement form for recovery password.

---
+++ Site/js/restore-password.js
@@ -0,0 +1,59 @@
+var API_ENDPOINT = 'http://localhost:50302/';
+// var API_ENDPOINT = 'https://api.campus.kpi.ua/';
+
+$(document).ready(function() {
+    $(".step-2").hide();
+    $(".step-3").hide();
+    $(".step-4").hide();
+});
+
+function restorePassword() {
+
+    var emailOrLogin = $('#user-id').val();
+    var captcha = $('#captcha-value').val();
+    var url = API_ENDPOINT + 'Account/Recovery/' + emailOrLogin + '?captcha=' + captcha;
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function(response) {
+            $(".step-2").hide();
+            $(".step-3").show();
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            if (xhr.status == 403) {
+                showMessage("Невiрний код пiдтвердження");
+                $('#captcha-value').val('');
+                getCaptcha();
+            }
+            if (xhr.status == 404) {
+                showMessage("Користувач з таким логiном, або електроною поштою не знайдений");
+                location.reload();
+            }
+            if (xhr.status == 409) {
+                $(".step-2").hide();
+                $(".step-3").hide();
+                $(".step-4").show();
+            }
+        }
+    });
+}
+
+function validate() {
+    $("#btn-get-captcha").prop("disabled", !$('#user-id').val());
+    $("#btn-get-password").prop("disabled", !$('#captcha-value').val());
+}
+
+function getCaptcha() {
+    $(".step-1").hide();
+    $(".step-2").show();
+
+    var emailOrLogin = $('#user-id').val();
+
+    var url = API_ENDPOINT + 'Account/Recovery/' + emailOrLogin + '/token?d=' + new Date().getTime();
+    $("#captcha-image").attr("src", url);
+}
+
+function showMessage(message) {
+    alert(message);
+}
