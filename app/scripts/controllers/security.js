635ee2c2535f6e3f666cfa355334739105218b4b
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-08-13 01:02:43 +0300

    Updage imported code for recovery password

--- app/scripts/controllers/security.js
+++ app/scripts/controllers/security.js
@@ -8,80 +8,72 @@
  * Controller of the ecampusApp
  */
 angular.module('ecampusApp')
-    .controller('SecurityCtrl', function() {
-        this.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('SecurityCtrl', function($scope) {
+
+        $scope.step = 1;
+        $scope.captcha = '';
+        $scope.userId = '';
+        $scope.loader = false;
+        $scope.captchaImage = '';
+
+        function restorePassword() {
+
+            var url = 'Account/Recovery';
+
+            var payload = {
+                Captcha: $scope.captcha,
+                UserIdentifier: $scope.userId
+            };
+
+            $scope.loader = true;
+
+            Campus.execute("POST", url, JSON.stringify(payload))
+                .done(function(result) {
+                    step(3);
+                    $scope.loader = false;
+                    $scope.$apply();
+                })
+                .fail(function(result) {
+                    $scope.loader = false;
+
+                    if (result.status === 403) {
+                        showMessage("Невiрний код пiдтвердження");
+                        $scope.captcha = '';
+                        getCaptcha();
+                    } else if (result.status === 404) {
+                        showMessage("Користувач з таким логiном, або електроною поштою не знайдений");
+                        location.reload();
+                    } else if (result.status === 409) {
+                        step(4);
+                    }
+
+                    $scope.$apply();
+                });
+        }
+
+        function getCaptcha() {
+            step(2);
+            $scope.captchaImage = Campus.ApiEndpoint + 'Account/Recovery/' + encodeURIComponent($scope.userId) + '/token?d=' + new Date().getTime();
+        }
+
+        function showMessage(message) {
+            alert(message);
+        }
+
+        function step(n) {
+            $scope.step = n;
+        }
+
+        function reload() {
+            step(1);
+        }
+
+        $scope.getCaptcha = function() {
+            getCaptcha();
+        }
+
+        $scope.restorePassword = function() {
+            restorePassword();
+        }
+
     });
-
-
-
-
-/*
-
-$(document).ready(function() {
-    step(1);
-});
-
-function step(n) {
-    for (var i = 1; i <= 4; i++) {
-        $(".step-" + i).hide();
-    }
-
-    $(".step-" + n).show();
-}
-
-function restorePassword() {
-
-    var url = 'Account/Recovery';
-
-    var payload = {
-        Captcha: $('#captcha-value').val(),
-        UserIdentifier: $('#user-id').val()
-    };
-
-    $(".loader").show();
-
-    Campus.execute("POST", url, JSON.stringify(payload))
-        .done(function(result) {
-            step(3);
-            $(".loader").hide();
-        })
-        .fail(function(result) {
-            $(".loader").hide();
-
-            if (result.status === 403) {
-                showMessage("Невiрний код пiдтвердження");
-                $('#captcha-value').val('');
-                getCaptcha();
-            }
-            if (result.status === 404) {
-                showMessage("Користувач з таким логiном, або електроною поштою не знайдений");
-                location.reload();
-            }
-            if (result.status === 409) {
-                step(4);
-            }
-        });
-}
-
-function validate() {
-    $("#btn-get-captcha").prop("disabled", !$('#user-id').val());
-    $("#btn-get-password").prop("disabled", !$('#captcha-value').val());
-}
-
-function getCaptcha() {
-    step(2);
-
-    var emailOrLogin = $('#user-id').val();
-
-    var url = Campus.ApiEndpoint + 'Account/Recovery/' + encodeURIComponent(emailOrLogin) + '/token?d=' + new Date().getTime();
-    $("#captcha-image").attr("src", url);
-}
-
-function showMessage(message) {
-    alert(message);
-}
-*/
