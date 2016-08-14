fd772409a03f7815b8ed76de8c5478952aceb390
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-08-14 23:24:20 +0300

    Update voting module

--- app/scripts/controllers/voting.js
+++ app/scripts/controllers/voting.js
@@ -8,10 +8,33 @@
  * Controller of the ecampusApp
  */
 angular.module('ecampusApp')
-  .controller('VotingCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('VotingCtrl', function($scope, $location, Api) {
+
+        $scope.currentUser = null;
+        $scope.personsForVote = [];
+
+        function reload() {
+
+            $scope.currentUser = Api.getCurrentUser();
+
+            if (!$scope.currentUser) {
+                $location.path("/login.html");
+            }
+
+            var payload = {
+                page: 1,
+                size: 100,
+                envelope: false
+            };
+
+            var action = "Vote/Persons/" + $scope.currentUser.id;
+
+            Api.execute("GET", action, payload).then(function(data) {
+                $scope.personsForVote = data;
+                $scope.$apply();
+            });
+        }
+
+        reload();
+
+    });
