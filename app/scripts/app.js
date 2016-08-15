a9bdf33668c3e42782fe7ca091bfcc9c6e14e009
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-08-15 10:21:06 +0300

    Add rating directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,52 +9,53 @@
  * Main module of the application.
  */
 var app = angular.module('ecampusApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'angular-input-stars'
 ]);
 
-app.config(function($routeProvider, $locationProvider) {
+app.config(function ($routeProvider, $locationProvider) {
 
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl',
-            controllerAs: 'main'
-        })
-        .when('/about.html', {
-            templateUrl: 'views/about.html',
-            controller: 'AboutCtrl',
-            controllerAs: 'about'
-        })
-        .when('/login.html', {
-            templateUrl: 'views/login.html',
-            controller: 'SecurityCtrl',
-            controllerAs: 'Security'
-        })
-        .when('/restore-password.html', {
-            templateUrl: 'views/restore-password.html',
-            controller: 'SecurityCtrl',
-            controllerAs: 'Security'
-        })
-        .when('/voting.html', {
-            templateUrl: 'views/voting.html',
-            controller: 'VotingCtrl',
-            controllerAs: 'Voting'
-        })
-        .when('/debugger.html', {
-            templateUrl: 'views/debugger.html',
-            controller: 'DebuggerCtrl',
-            controllerAs: 'Debugger'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+    .when('/about.html', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl',
+      controllerAs: 'about'
+    })
+    .when('/login.html', {
+      templateUrl: 'views/login.html',
+      controller: 'SecurityCtrl',
+      controllerAs: 'Security'
+    })
+    .when('/restore-password.html', {
+      templateUrl: 'views/restore-password.html',
+      controller: 'SecurityCtrl',
+      controllerAs: 'Security'
+    })
+    .when('/voting.html', {
+      templateUrl: 'views/voting.html',
+      controller: 'VotingCtrl',
+      controllerAs: 'Voting'
+    })
+    .when('/debugger.html', {
+      templateUrl: 'views/debugger.html',
+      controller: 'DebuggerCtrl',
+      controllerAs: 'Debugger'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 
-    $locationProvider.html5Mode(true);
+  //$locationProvider.html5Mode(true);
 
-    //Campus.setApiEndpoint('https://api-campus-kpi-ua.azurewebsites.net/');
+  Campus.setApiEndpoint('https://api-campus-kpi-ua.azurewebsites.net/');
 });
--- app/scripts/controllers/voting.js
+++ app/scripts/controllers/voting.js
@@ -8,33 +8,70 @@
  * Controller of the ecampusApp
  */
 angular.module('ecampusApp')
-    .controller('VotingCtrl', function($scope, $location, Api) {
+  .controller('VotingCtrl', function ($scope, $location, Api) {
 
-        $scope.currentUser = null;
-        $scope.personsForVote = [];
+    $scope.currentUser = null;
+    $scope.personsForVote = [];
 
-        function reload() {
+    function reload() {
 
-            $scope.currentUser = Api.getCurrentUser();
+      $scope.currentUser = Api.getCurrentUser();
 
-            if (!$scope.currentUser) {
-                $location.path("/login.html");
-            }
+      if (!$scope.currentUser) {
+        $location.path("/login.html");
+      }
 
-            var payload = {
-                page: 1,
-                size: 100,
-                envelope: false
-            };
+      Api.execute("GET", "Vote/Term/Current").then(function (data) {
+        $scope.voteTerm = !!data && data.length > 0 ? data[0] : null;
 
-            var action = "Vote/Persons/" + $scope.currentUser.id;
-
-            Api.execute("GET", action, payload).then(function(data) {
-                $scope.personsForVote = data;
-                $scope.$apply();
-            });
+        if (!$scope.voteTerm) {
+          //Voting is closed
         }
 
-        reload();
+        getAllPersons();
+        getAlreadyVotedPersons();
 
-    });
+      });
+
+      function getAllPersons() {
+        var action = "Vote/Persons/" + $scope.currentUser.id;
+
+        var payload = {
+          page: 1,
+          size: 100,
+          envelope: false
+        };
+
+        Api.execute("GET", action, payload).then(function (data) {
+          $scope.personsForVote = data;
+          $scope.$apply();
+
+          console.log('$scope.personsForVote', $scope.personsForVote);
+        });
+      }
+
+      function getAlreadyVotedPersons() {
+
+        var action = "Vote/Persons/" + $scope.currentUser.id + "/Voted";
+
+        var payload = {
+          voteTermId: $scope.voteTerm.id,
+          page: 1,
+          size: 100,
+          envelope: false
+        };
+
+        Api.execute("GET", action, payload).then(function (data) {
+          $scope.personsAlreadyVoted = data;
+          $scope.$apply();
+
+          console.log('$scope.personsAlreadyVoted', $scope.personsAlreadyVoted);
+        });
+
+      }
+
+    }
+
+    reload();
+
+  });
