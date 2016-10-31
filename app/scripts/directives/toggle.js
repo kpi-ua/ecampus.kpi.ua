b08c825a4cc518b0a41e53dc534073f0ae7f70c7
bav2212 <bav2212@gmail.com>
2016-10-31 15:51:01 +0200

    add toggle directive(for correct bootstrap toogle work)

---
+++ app/scripts/directives/toggle.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name ecampusApp.directive:toggle
+ * @description
+ * # toggle
+ */
+angular.module('ecampusApp')
+  .directive('toggle', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        if (attrs.toggle == "tooltip") {
+          $(element).tooltip();
+        }
+      }
+    };
+  });
