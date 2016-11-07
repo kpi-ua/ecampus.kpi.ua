7e2ce510d025fef1904df25bc4821cca74a52772
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2016-11-07 19:47:09 +0200

    Removed authorized element. Fixed logo position.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,8 @@ module.exports = function(config) {
       'bower_components/kjur-jsrsasign/jsrsasign-latest-all-min.js',
       'bower_components/jwt-decode/build/jwt-decode.js',
       'bower_components/webix/codebase/webix.js',
+      'bower_components/excellentexport/excellentexport.js',
+      'bower_components/page-scroll-to-id/jquery.malihu.PageScroll2id.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
