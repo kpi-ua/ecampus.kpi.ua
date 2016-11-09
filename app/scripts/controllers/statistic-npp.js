189191440a12eaf6db1abb3f9dff7c0725f456a3
MizovOO <MizovOO@ua.fm>
2016-11-09 21:31:30 +0200

    remove decode Token logic to separate file

--- app/scripts/controllers/statistic-npp.js
+++ app/scripts/controllers/statistic-npp.js
@@ -32,25 +32,6 @@ angular.module('ecampusApp')
             }
         }
 
-        function decodeToken(accessTokenIn) {
-
-            if (!accessTokenIn || accessTokenIn == 'null') {
-                return null;
-            }
-
-            var a = accessTokenIn.split(".");
-            var uHeader = b64utoutf8(a[0]);
-            var uClaim = b64utoutf8(a[1]);
-
-            var pHeader = KJUR.jws.JWS.readSafeJSONString(uHeader);
-            var pClaim = KJUR.jws.JWS.readSafeJSONString(uClaim);
-
-            var sHeader = JSON.stringify(pHeader, null, "  ");
-            var sClaim = JSON.stringify(pClaim, null, "  ");
-
-            return sClaim;
-        }
-
         function getParent(obj, parentTagName) {
             return (obj.tagName == parentTagName) ? obj : getParent(obj.parentNode, parentTagName);
         }
