98d1f2562388431a27e57dbabe142d60b5119b0d
KovalenkoStas <staxa1995@gmail.com>
2016-11-27 22:48:42 +0200

    implemented review attestation results for groups mode

--- app/scripts/controllers/attestation.js
+++ app/scripts/controllers/attestation.js
@@ -12,11 +12,21 @@ angular.module('ecampusApp')
     $scope.errorMessageYears = '';
     $scope.errorMessageAttests = '';
     $scope.errorMessageGroups = '';
-    $scope.attestPeriodId = '';
+    $scope.errorLoadGroupsResult = '';
+    $scope.attestationPeriodId = null;
+    $scope.getGroupsResults = false;
+    $scope.disciplinesList = [];
+
     $scope.Semesters = [
       {id: 1, name: 'Перший семестр'},
       {id: 2, name: 'Другий семестр'}
     ];
+
+    function disciplinesTeachersModel(disciplineName, disciplineShortName, teacherName) {
+      this.disciplineFullName = disciplineName;
+      this.disciplineShortName = disciplineShortName;
+      this.teacherName = teacherName;
+    }
 
     $scope.setPill = function (newPill) {
       $scope.pill = newPill;
@@ -25,7 +35,41 @@ angular.module('ecampusApp')
     $scope.isSet = function (pillNum) {
       return $scope.pill === pillNum;
     };
-    
+
+    function getDisciplineName(fullName) {
+      var result = fullName.split(",");
+      return result[0];
+    }
+
+    function uniqueElements(arr) {
+      var result = [];
+
+      nextInput:
+        for (var i = 0; i < arr.length; i++) {
+          var str = arr[i];
+          for (var j = 0; j < result.length; j++) {
+            if ((result[j].disciplineFullName == str.disciplineFullName)
+              && (result[j].teacherName == str.teacherName)
+              && (result[j].disciplineShortName == str.disciplineShortName)) {
+              continue nextInput;
+            }
+          }
+          result.push(str);
+        }
+      return result;
+    }
+
+
+    function sortByStudentNameThenDiscName(a, b) {
+      var name1 = a.student;
+      var name2 = b.student;
+
+      var disc1 = a.disciplineFullName;
+      var disc2 = b.disciplineFullName;
+
+      return name1.localeCompare(name2) || disc1.localeCompare(disc2);
+    }
+
     function LoadYears() {
       var url = 'Attestation/studyYear';
       Api.execute("GET", url)
@@ -39,7 +83,7 @@ angular.module('ecampusApp')
     }
 
     function generateUrlForAttestPeriod(year, semester, attestNum) {
-      return 'Attestation/period?dcStudingYearId=' + year + '&semesterYear=' + semester +'&dcLoadId=' + attestNum;
+      return 'Attestation/period?dcStudingYearId=' + year + '&semesterYear=' + semester + '&dcLoadId=' + attestNum;
     }
 
     function LoadAttests() {
@@ -56,13 +100,12 @@ angular.module('ecampusApp')
 
     $scope.getAttestationPeriodId = function (dcStudingYearId, semesterYear, dcLoadId) {
       var url = generateUrlForAttestPeriod(dcStudingYearId, semesterYear, dcLoadId);
-      console.log(url);
       Api.execute("GET", url)
         .then(function (response) {
-            $scope.attestationPeriodId = response;
+            $scope.attestationPeriodId = +(response);
           },
           function () {
-            $scope.Attests = null;
+            $scope.attestationPeriodId = null;
           });
     };
 
@@ -84,6 +127,29 @@ angular.module('ecampusApp')
         $scope.errorMessageGroups = "Введіть більше 2-х символів для пошуку групи";
       }
     };
+
+    $scope.LoadGroupsResult = function (rtStudyGroupId, cAttestationPeriodId) {
+      var url = 'Attestation/group/' + rtStudyGroupId + '/period/' + cAttestationPeriodId + '/result';
+      Api.execute("GET", url)
+        .then(function (response) {
+            $scope.errorLoadGroupsResult = "";
+            $scope.GroupsResult = response.sort(sortByStudentNameThenDiscName);
+            console.log(response);
+            var allDisciplinesList = [];
+            for (var i = 0; i < response.length; i++) {
+              allDisciplinesList.push(
+                new disciplinesTeachersModel(response[i].disciplineFullName,
+                  getDisciplineName(response[i].disciplineFullName),
+                  response[i].teacher));
+            }
+            $scope.disciplinesList = uniqueElements(allDisciplinesList);
+          },
+          function () {
+            $scope.errorLoadGroupsResult = "Не вдалося завантажити результати для даної групи";
+            $scope.GroupsResult = null;
+          });
+    };
+
 
     LoadYears();
     LoadAttests();
