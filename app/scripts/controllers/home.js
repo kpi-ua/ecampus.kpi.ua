aee0c72d47e0b6f43945c68964d23c7a5b4ddab6
KovalenkoStas <staxa1995@gmail.com>
2017-01-13 13:58:02 +0200

    implement removing user token and unlogin user when session time is expired

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,6 +8,13 @@
  * Controller of the ecampusApp
  */
 angular.module('ecampusApp')
-    .controller('HomeCtrl', function() {
-
+  .controller('HomeCtrl', function (Api, $scope) {
+    $scope.sessionExpired = false;
+    angular.element(document).ready(function () {
+      var isLogged = Api.getToken();
+      if (isLogged) {
+        $scope.sessionExpired = Api.removeToken();
+        $scope.$apply();
+      }
     });
+  });
