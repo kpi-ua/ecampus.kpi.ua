dca8cb354ddca7c310c2185068b7411494eceeb8
KovalenkoStas <staxa1995@gmail.com>
2017-01-13 14:38:30 +0200

    refactor removing user token and unlogin user (run this function on any page, before only on home module)

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,13 +8,6 @@
  * Controller of the ecampusApp
  */
 angular.module('ecampusApp')
-  .controller('HomeCtrl', function (Api, $scope) {
-    $scope.sessionExpired = false;
-    angular.element(document).ready(function () {
-      var isLogged = Api.getToken();
-      if (isLogged) {
-        $scope.sessionExpired = Api.removeToken();
-        $scope.$apply();
-      }
-    });
+  .controller('HomeCtrl', function () {
+
   });
