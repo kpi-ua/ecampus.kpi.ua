a38a8a4c08f0f9fd3ca7fbbfc1675e113a07674b
MizovOO <MizovOO@ua.fm>
2017-02-02 16:51:42 +0200

    Add new directive rnp-input, which contain all inputs for rnp controller

---
+++ app/scripts/directives/rnp-input.js
@@ -0,0 +1,12 @@
+angular.module('ecampusApp')
+    .directive('rnpInput',function () {
+        return{
+            link: function (scope, element, attrs) {
+
+
+            },
+            templateUrl: 'views/directives/rnp-input.html',
+            restrict : 'EA',
+
+        }
+    });
