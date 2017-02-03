bc89bd8dbca48b6ab2ca6800f3cfe1d5ccea9cbb
MizovOO <MizovOO@ua.fm>
2017-02-03 09:35:43 +0200

    add directive rnp-rows-display

---
+++ app/scripts/directives/rnp-rows-display.js
@@ -0,0 +1,17 @@
+angular.module('ecampusApp').directive('rnpRowsDisplay', function () {
+    return {
+        templateUrl: 'views/directives/rnp-rows-display.html',
+        restrict: 'E',
+        link: function ($scope, element, attr) {
+
+            $scope.data = $scope[attr["rows"]];
+            var watch = attr["rows"];
+            $scope.$watch(watch, function () {
+                $scope.data = $scope[attr["rows"]];
+            });
+
+
+
+        }
+    }
+});
