c090022d940a288f0e18d95c6d4bc82939cd52ad
MizovOO <MizovOO@ua.fm>
2017-02-06 12:58:42 +0200

    bound API and rnp-input directive

--- app/scripts/controllers/rnp.js
+++ app/scripts/controllers/rnp.js
@@ -15,36 +15,27 @@ angular.module('ecampusApp')
       'Karma'
     ];
       $scope.options ={
-          StudyYears: [
-              {
-              "name": "2015-2016",
-              "id": 5
-              },
-              {
-              "name": "2016-2017",
-              "id": 6
-              }
-          ],
-          Departments : [
-          {
-              "mark": 1,
-              "name": "Кафедра автоматизованих систем обробки інформації та управління ФІОТ",
-              "id": 10120
-          },{
-              "mark": 2,
-              "name": "Кафедра інформації та управління ФІОТ",
-              "id": 10122
-          }
-          ],
+          StudyYears: [],
+          Departments : [],
           Okrs: [],
           Specializations: [],
           StudyForms: [],
           XmlCodes: []
       };
 
+      $scope.model = {
+          studyYearName:null,
+          departmentItem:null,
+          okrName:null,
+          specializationCodeName:null,
+          studyFormName:null,
+          xmlCode:null
+      };
+
       $scope.selectData={
-          studyYear:null,
+          studyYearId:null,
           departmentId:null,
+          departmentMark:null,
           okrId:null,
           specializationId:null,
           studyFormId:null,
@@ -54,67 +45,14 @@ angular.module('ecampusApp')
 
       $scope.display= {
           StudyGroups: [],
-          //     {
-          //         "name": "ІК-41",
-          //         "id": 8503
-          //     },
-          //     {
-          //         "name": "ІК-31",
-          //         "id": 8502
-          //     },
-          //     {
-          //         "name": "ІК-21",
-          //         "id": 8501
-          //     },
-          // ],
           RnpRows: []
-          //     {
-          //         "cycleName": "Цикл професійної та практичної підготовки",
-          //         "rnpRows": [
-          //             {
-          //                 "rnp": {
-          //                     "name": "Теорія автоматичного управління - 2. Теорія цифрових систем управління",
-          //                     "id": 98479
-          //                 },
-          //                 "subdivisionName": "Кафедра технічної кібернетики ФІОТ",
-          //                 "credits": 3,
-          //                 "countLecture": 36,
-          //                 "countPractice": 0,
-          //                 "countLaboratory": 18,
-          //                 "independentStudentWork": 51,
-          //                 "cycleName": "Цикл професійної та практичної підготовки"
-          //             }
-          //         ]
-          //
-          //     },
-          //     {
-          //         "cycleName": "Цикл професійної та практичної підготовки 2",
-          //         "rnpRows": [
-          //             {
-          //                 "rnp": {
-          //                     "name": "Теорія автоматичного управління - 2. Теорія цифрових систем управління",
-          //                     "id": 98479
-          //                 },
-          //                 "subdivisionName": "Кафедра технічної кібернетики ФІОТ",
-          //                 "credits": 3,
-          //                 "countLecture": 36,
-          //                 "countPractice": 0,
-          //                 "countLaboratory": 18,
-          //                 "independentStudentWork": 51,
-          //                 "cycleName": "Цикл професійної та практичної підготовки"
-          //             }
-          //         ]
-          //
-          //     }
-          // ],
       };
       $scope.RnpRows = null;
+      $scope.errorLabelText="";
       // $scope.RnpRows = $scope.display.RnpRows;
 
       onInit();
       function onInit(){
-          $scope.selectData.studyYear = $scope.options.StudyYears[0].name;
-          $scope.selectData.departmentId =$scope.options.Departments[0];
           // $scope.selectData.departmentId = $scope.options.Departments [0].id;
           // $scope.selectData.okrId = $scope.options.Okrs[0].id;
           // $scope.selectData.specializationId = $scope.options.Specializations[0].id;
