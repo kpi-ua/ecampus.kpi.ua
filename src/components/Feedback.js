1124d06f0c59e94ff115edae915ccd2e395b7331
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2020-04-04 17:35:50 +0300

    Update feedback form height

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -9,7 +9,7 @@ class Feedback extends Component {
         <div className="col-md-12">
           <h1>Форма скарг i пропозицiй</h1>
 
-          <iframe width="100%" height="1000px"
+          <iframe width="100%" height="1400px"
                   src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZ__pj8rjRUOUFNWTNVU0VIWFlRWTZXNzRaVklaUUVPTS4u&embed=true"
                   frameBorder="0" marginWidth="0" marginHeight="0"
                   style={{border: "none", maxWidth: "100%", maxHeight: "100vh"}}
