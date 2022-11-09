4a4d1cf81638844519ad2deee4ab3111b9bb8eef
Andrew <andrew.gubskiy@outlook.com>
2022-11-09 18:34:12 +0200

    Update configuration loading

--- src/ApplicationConfiguration.js
+++ src/ApplicationConfiguration.js
@@ -1,6 +1,10 @@
 class ApplicationConfiguration {
     constructor() {
-        this.configuratiun = require('/config.json');
+        //this.configuratiun = require('./config.json');
+
+        const json = this.loadTextFileAjaxSync('/config.json');
+        this.configuratiun = JSON.parse(json);
+
         console.log(this.configuratiun);
     }
 
@@ -15,6 +19,24 @@ class ApplicationConfiguration {
     get LoginPageAddress() {
         return this.configuratiun.LoginPageAddress;
     }
+
+    loadTextFileAjaxSync(filePath)
+    {
+        const xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET",filePath,false);
+
+        xmlhttp.send();
+
+        if (xmlhttp.status==200 && xmlhttp.readyState == 4 )
+        {
+            return xmlhttp.responseText;
+        }
+        else {
+            // TODO Throw exception
+            return null;
+        }
+    }
 }
+
 
 export default new ApplicationConfiguration();
