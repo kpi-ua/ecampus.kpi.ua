f7298858242a3c2fdf7347f7b4c438eb4eb29ccc
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-10-31 18:41:05 +0200

    KB-84 Added favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,23 @@
 import "./globals.css";
 
-import { Exo_2 } from "next/font/google";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from 'next-intl';
+import { exo2Font } from './font';
 import { getMessages } from 'next-intl/server';
-
-const exo2Font = Exo_2({ subsets: ['cyrillic', 'latin'], weight: ['400', '500', '600'] });
 
 export const metadata: Metadata = {
   title: "Кампус КПІ",
   description: "Кампус КПІ",
+  icons: [
+    { type: 'image/png', sizes: '48x48', url: '/favicon-48x48.png' },
+    { type: 'image/svg+xml', url: '/favicon.svg' },
+    { rel: 'shortcut icon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', url: '/apple-touch-icon.png' },
+  ],
+  manifest: '/site.webmanifest',
+  appleWebApp: {
+    title: 'Е-Кампус',
+  }
 };
 
 export default async function RootLayout({
@@ -20,7 +28,7 @@ export default async function RootLayout({
   const messages = await getMessages();
 
   return (
-    <html lang="en">
+    <html>
       <body className={`${exo2Font.className}`}>
         <NextIntlClientProvider messages={messages}>
           {children}
