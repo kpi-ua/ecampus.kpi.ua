c50eb1f4e676a2797b1860afb7214b9ab4923fd7
Andrew Izvarin <andrew.izvarin@gmail.com>
2024-12-04 19:48:34 +0200

    KB-85 use next image component

--- src/app/[locale]/(private)/profile-picture.tsx
+++ src/app/[locale]/(private)/profile-picture.tsx
@@ -5,6 +5,7 @@ import { useLocalStorage } from '@/hooks/use-storage';
 import { User } from '@/types/user';
 import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 import { CircleUserRound } from 'lucide-react';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 export const ProfilePicture = () => {
@@ -20,7 +21,7 @@ export const ProfilePicture = () => {
       <AspectRatio ratio={1 / 1}>
         <Show when={isClient}>
           {user?.photo
-            ? <img src={user.photo} alt={user.fullName} className="rounded-full" />
+            ? <Image src={user.photo} alt={user.fullName} width={48} height={48} className="rounded-full" />
             : <CircleUserRound width={48} height={48} className="text-basic-blue" strokeWidth={1} />
           }
         </Show>
