501f6fae61a039d38b8e919eac12ee76d86ae86b
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-12-10 14:09:33 +0200

    Add handling kid parameter

--- src/components/AuthContainerDefault.js
+++ src/components/AuthContainerDefault.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import AuthForm from './AuthForm';
 import * as campus from '../CampusClient';
-import { useHistory } from 'react-router-dom';
-import ApplicationConfiguration from './ApplicationConfiguration';
+import { useLocation, useHistory } from 'react-router-dom';
+import ApplicationConfiguration from '../ApplicationConfiguration';
 
 const AuthContainerDefault = () => {
   const [authFail, setAuthFail] = useState(false);
   const history = useHistory();
+  const location = useLocation();
+
+  // Parse query parameters
+  const queryParams = new URLSearchParams(location.search);
+  const showKpiIdAuthButton = queryParams.get('kid') === 'true';
 
   const authorize = async (e, { login, password }) => {
     e.preventDefault();
@@ -28,14 +33,19 @@ const AuthContainerDefault = () => {
       authFail={authFail}
       dismissInvalid={dismissInvalid}
     >
-      <div className="form-group">
-        <a className="btn btn-block btn-social btn-kpi-id" href={ApplicationConfiguration.KpiIdRedirectAddress}>
-          <div className="icon">
-            <span className="fa fa-key"/>
-          </div>
-          Увiйти через KPI ID
-        </a>
-      </div>
+      {showKpiIdAuthButton && (
+        <div className="form-group">
+          <a
+            className="btn btn-block btn-social btn-kpi-id"
+            href={ApplicationConfiguration.KpiIdRedirectAddress}
+          >
+            <div className="icon">
+              <span className="fa fa-key" />
+            </div>
+            Увiйти через KPI ID
+          </a>
+        </div>
+      )}
     </AuthForm>
   );
 };
