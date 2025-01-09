ffe2e586c68fc0e8bd19d252c2f2d1ad66c47893
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2025-01-09 22:33:48 +0200

    Update links

--- src/app/[locale]/(private)/frequently-asked-questions/page.tsx
+++ src/app/[locale]/(private)/frequently-asked-questions/page.tsx
@@ -3,6 +3,8 @@ import { SubLayout } from '../sub-layout';
 import { useTranslations } from 'next-intl';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { Link } from '@/i18n/routing';
+import dayjs from 'dayjs';
 
 const SECTIONS = [
   'how-to-register',
@@ -33,16 +35,57 @@ export default function FAQPage({ params: { locale } }: { params: { locale: stri
       <div className="col-span-6">
         <Heading1>{t('title')}</Heading1>
         <Accordion type="multiple" className="mt-8">
-          {SECTIONS.map((section) => (
-            <AccordionItem value={section} key={section}>
-              <AccordionTrigger>
-                <Heading4 className="my-0">{t(`sections.${section}.header`)}</Heading4>
-              </AccordionTrigger>
-              <AccordionContent className="ml-12 text-lg">{t.rich(`sections.${section}.content`)}</AccordionContent>
-            </AccordionItem>
-          ))}
+          {SECTIONS.map((section) => {
+            const header = t(`sections.${section}.header`);
+            const content = t.rich(`sections.${section}.content`, {
+              documentlink: (chunks) => (
+                <Link href="https://document.kpi.ua/2024_RP-294" target="_blank">
+                  {chunks}
+                </Link>
+              ),
+              curatorlnik: (chunks) => (
+                <Link href={process.env.NEXT_PUBLIC_CAMPUS_FIND_CURATOR!} target="_blank">
+                  {chunks}
+                </Link>
+              ),
+              restorepasswordlink: (chunks) => (
+                <Link href={process.env.NEXT_PUBLIC_CAMPUS_RESTORE_PASSWORD!} target="_blank">
+                  {chunks}
+                </Link>
+              ),
+            });
+            return (
+              <AccordionItem value={section} key={section} className="mb-4">
+                <AccordionTrigger className="font-bold text-lg">{header}</AccordionTrigger>
+                <AccordionContent className="ml-12 text-base">{content}</AccordionContent>
+              </AccordionItem>
+            );
+          })}
         </Accordion>
       </div>
+
+      {/* Footer */}
+      <footer className="mt-8">
+        {t.rich('footer', {
+          kbislink: (chunks) => (
+            <Link href={process.env.NEXT_PUBLIC_KBIS_URL!} target="_blank">
+              {chunks}
+            </Link>
+          ),
+          year: dayjs().year(),
+        })}
+      </footer>
+
+      {/* Retry Message */}
+      <p className="text-neutral-600 mt-4">
+        {t.rich('retryMessage', {
+          link: (chunks) => (
+            <Link href={{ pathname: '/password-reset', query: { username: 'example' } }}>
+              {chunks}
+            </Link>
+          ),
+        })}
+      </p>
     </SubLayout>
   );
 }
