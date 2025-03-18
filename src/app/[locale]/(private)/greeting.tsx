e444783c3ec2f76ebffa524f2b0e64bdb5e540f4
Andrew Izvarin <andrew.izvarin@gmail.com>
2025-03-18 11:46:57 +0200

    KB-329 Use english user name for en locale, translate 'welcome' part

--- src/app/[locale]/(private)/greeting.tsx
+++ src/app/[locale]/(private)/greeting.tsx
@@ -1,6 +1,8 @@
 import { getUserDetails } from '@/actions/auth.actions';
 import { Heading1 } from '@/components/typography/headers';
+import { LOCALE } from '@/i18n/routing';
 import { cn } from '@/lib/utils';
+import { getLocale, getTranslations } from 'next-intl/server';
 
 interface GreetingProps {
   className?: string;
@@ -8,6 +10,14 @@ interface GreetingProps {
 
 export default async function Greeting({ className }: GreetingProps) {
   const user = await getUserDetails();
+  const locale = await getLocale();
+  const t = await getTranslations('private.main');
 
-  return <Heading1 className={cn(className)}>Вітаємо, {user?.fullName}!</Heading1>;
+  const fullName = locale === LOCALE.EN ? user?.fullNameEnglish : user?.fullName;
+
+  return (
+    <Heading1 className={cn(className)}>
+      {t('welcome')}, {fullName}!
+    </Heading1>
+  );
 }
