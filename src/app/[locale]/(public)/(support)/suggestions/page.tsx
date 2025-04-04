7eb796a148298a5e5faea4947e9d55d1445c22f9
Andrew Izvarin <andrew.izvarin@gmail.com>
2025-04-04 22:35:37 +0300

    KB-347 rename complaints form to suggestions form

--- src/app/[locale]/(public)/(support)/suggestions/page.tsx
+++ src/app/[locale]/(public)/(support)/suggestions/page.tsx
@@ -1,24 +1,24 @@
 import { useTranslations } from 'next-intl';
 import { SupportNavLayout } from '../support-nav-layout';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
-import { ComplaintsForm } from '@/components/complaints-form';
+import { SuggestionsForm } from '@/components/suggestions-form';
 
 export async function generateMetadata({ params: { locale } }: { params: { locale: string } }) {
-  const t = await getTranslations({ locale, namespace: 'public.complaints' });
+  const t = await getTranslations({ locale, namespace: 'public.suggestions' });
 
   return {
     title: t('header'),
   };
 }
 
-export default function ComplaintsPage({ params: { locale } }: { params: { locale: string } }) {
+export default function SuggestionPage({ params: { locale } }: { params: { locale: string } }) {
   setRequestLocale(locale);
 
-  const t = useTranslations('public.complaints');
+  const t = useTranslations('public.suggestions');
 
   return (
     <SupportNavLayout header={t('header')} className="w-full grow">
-      <ComplaintsForm className="-mx-[5%] w-[110%]" />
+      <SuggestionsForm className="-mx-[5%] w-[110%]" />
     </SupportNavLayout>
   );
 }
