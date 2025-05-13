5b8fc3746bc37702ceb5e67be75e82245877068e
Markusplay <mshilo2004@gmail.com>
2025-05-14 00:53:51 +0300

    add term api

--- src/types/models/session.ts
+++ src/types/models/session.ts
@@ -1,11 +1,14 @@
 import { Lecturer } from '@/types/models/current-control/lecturer';
+import { AssessmentType } from '@/types/enums/session/assessment-type';
+import { RecordType } from '@/types/enums/session/record-type';
+import { Status } from '@/types/enums/session/status';
 
 export interface Session {
   name: string;
   mark?: number;
-  assessmentType: string;
-  recordType: string;
+  assessmentType: AssessmentType;
+  recordType: RecordType;
   date?: string;
-  status: string;
+  status: Status;
   lecturer: Lecturer;
 }
