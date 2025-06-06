387e8df7674f30dd18bc82a5836acccda119376a
Markusplay <mshilo2004@gmail.com>
2025-06-06 14:01:46 +0300

    fix current module structure

--- src/app/[locale]/(private)/module/studysheet/[id]/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Journal } from '@/types/models/current-control/journal';
-
-export const getTotalScore = (journal: Journal[]) =>
-  journal.reduce((acc, entry) => {
-    const score = typeof entry.score === 'number' ? entry.score : 0;
-    return acc + score;
-  }, 0);
--- src/types/models/current-control/journal.ts
+++ src/types/models/current-control/journal.ts
@@ -1,10 +1,15 @@
 import { Lecturer } from '@/types/models/lecturer';
 
-export interface Journal {
+interface JournalDiscipline {
   date: string;
   presence: boolean;
   score?: number;
   controlType: string;
   lecturer: Lecturer;
   note: string;
+}
+
+export interface Journal {
+  disciplines: JournalDiscipline[];
+  totalScore: number;
 }
