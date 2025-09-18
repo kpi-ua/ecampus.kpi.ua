3860c2e79e463e41aa5cceae4d312d9302cd90df
Markusplay <mshilo2004@gmail.com>
2025-09-19 01:59:36 +0300

    fix 404 error in curator module

--- src/actions/curator.actions.ts
+++ src/actions/curator.actions.ts
@@ -1,8 +1,12 @@
 import { campusFetch } from '@/lib/client';
 import { Curator } from '@/types/models/curator';
 
-export async function getCurator() {
+export async function getCurator(): Promise<Curator | null> {
   const response = await campusFetch<Curator>('/curator');
+  if (response.status === 404) {
+    return null;
+  }
+
   if (!response.ok) {
     throw new Error(`${response.status} Error`);
   }
