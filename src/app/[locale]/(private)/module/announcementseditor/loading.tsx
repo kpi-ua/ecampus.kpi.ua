bef00896df684cef5d0c4d8642ae94c001ba8634
Oleg Zapara <ole9zp@gmail.com>
2025-11-11 00:37:01 +0200

    add announcement editor loading

---
+++ src/app/[locale]/(private)/module/announcementseditor/loading.tsx
@@ -0,0 +1,9 @@
+import SpinnerGap from '../../../../images/icons/SpinnerGap.svg';
+
+export default function Loading() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <SpinnerGap />
+    </div>
+  );
+}
