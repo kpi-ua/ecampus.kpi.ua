d7cec0e70942e48ec99d63342608b9e3428e920a
Oleg Zapara <ole9zp@gmail.com>
2025-11-13 20:15:18 +0200

    add common LoadingScreen component

--- src/app/[locale]/(private)/module/vedomoststud/loading.tsx
+++ src/app/[locale]/(private)/module/vedomoststud/loading.tsx
@@ -1,9 +1,5 @@
-import SpinnerGap from '../../../../images/icons/SpinnerGap.svg';
+import { LoadingScreen } from '@/components/loading-screen';
 
 export default function Loading() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <SpinnerGap />
-    </div>
-  );
+  return <LoadingScreen />;
 }
---
+++ src/components/loading-screen.tsx
@@ -0,0 +1,9 @@
+import { SpinnerGap } from '@/app/images';
+
+export const LoadingScreen = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <SpinnerGap />
+    </div>
+  );
+};
