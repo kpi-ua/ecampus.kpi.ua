7db326caea155f07ef605f053372c544e2acc89f
Andrey Gubskiy <andrew.gubskiy@outlook.com>
2025-12-15 01:30:06 +0200

    rollback menu.actions.ts

--- src/types/module.ts
+++ src/types/module.ts
@@ -3,4 +3,5 @@ import { ProfileArea } from '@/types/enums/profile-area';
 export interface Module {
   name: string;
   isExternal: boolean | ((profileArea: ProfileArea) => boolean);
+  group?: string;
 }
