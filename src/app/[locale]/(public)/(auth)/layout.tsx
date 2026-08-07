1b362e3b5c2c39c16cd12a86321fe8e4116d6e8b
Markusplay <mshilo2004@gmail.com>
2026-08-07 08:43:56 +0300

    center inner form container

--- src/app/[locale]/(public)/(auth)/layout.tsx
+++ src/app/[locale]/(public)/(auth)/layout.tsx
@@ -16,7 +16,7 @@ export default async function AuthLayout({ children, params }: Props) {
     <div className="3xl:!px-[167px] 3xl:!py-[48px] relative flex min-h-dvh min-w-[344px] flex-col px-5 py-5 md:px-14 md:py-8">
       <Header />
       <main className="3xl:tall:absolute 3xl:tall:inset-0 3xl:tall:items-center 3xl:tall:py-0 flex justify-center pt-8 pb-0 md:grow md:py-8">
-        <div className="w-full max-w-[516px]">{children}</div>
+        <div className="flex flex-col justify-center w-full max-w-[516px]">{children}</div>
       </main>
       <Footer className="3xl:tall:absolute 3xl:tall:right-[167px] 3xl:tall:bottom-[48px] 3xl:tall:left-[167px] 3xl:tall:mt-0 mt-[36px] text-center" />
     </div>
